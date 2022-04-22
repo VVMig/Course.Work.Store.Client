1b45379e5d1ce2925cf918deb4b2cc9d3b47d916
Vladimir <vladimir.migay@effective-soft.com>
2022-04-22 12:37:48 +0300

    Add max file size

--- src/components/Common/CustomFileUploader/index.tsx
+++ src/components/Common/CustomFileUploader/index.tsx
@@ -1,6 +1,7 @@
 import { UploadFile } from '@mui/icons-material';
 import React from 'react';
 import { FileUploader } from 'react-drag-drop-files';
+import { toast } from 'react-toastify';
 
 interface IProps {
     handleChange: (uploadedImages: Blob[]) => Promise<void>;
@@ -20,6 +21,10 @@ export const CustomFileUploader = ({
             types={fileTypes}
             classes="image-uploader"
             multiple
+            maxSize={10}
+            onSizeError={() =>
+                toast.error('Too large image. Max file size is 10mb')
+            }
         >
             <div className="image-uploader__tip">
                 <UploadFile />
